1e1e8ffc76e6a0fe69d5a9040b2ece507247dada
GiaHuy <74665412+giahuy200202@users.noreply.github.com>
2023-11-21 23:40:41 +0700

    handle UI list joined classroom

--- FRONTEND/customer/src/components/HomePage/HomePageContent.js
+++ FRONTEND/customer/src/components/HomePage/HomePageContent.js
@@ -1,32 +1,52 @@
-import "./HomePageContent.css";
+import styles from "./HomePageContent.module.css";
 import { RingLoader } from "react-spinners";
 import { useEffect, useContext } from "react";
 import { useLocation } from "react-router-dom";
 import AuthContext from "../../store/auth-context";
+import { ReactComponent as LeaveIcon } from "../../assests/svg/leave.svg";
+import { ReactComponent as FolderIcon } from "../../assests/svg/folder.svg";
 
 const HomePageContent = () => {
-
   // get information when login with social
   const authCtx = useContext(AuthContext);
   const location = useLocation();
   useEffect(() => {
-    const token = new URLSearchParams(location.search).get('token');
-    const expiresTime = new URLSearchParams(location.search).get('expiresTime');
-    const userData = new URLSearchParams(location.search).get('userData');
+    const token = new URLSearchParams(location.search).get("token");
+    const expiresTime = new URLSearchParams(location.search).get("expiresTime");
+    const userData = new URLSearchParams(location.search).get("userData");
     if (token) {
-      const expirationTime = new Date(
-        new Date().getTime() + +expiresTime
-      );
+      const expirationTime = new Date(new Date().getTime() + +expiresTime);
       authCtx.login(token, expirationTime.toISOString());
-      console.log(userData)
+      console.log(userData);
     }
   }, [location.search]);
 
+  const classData = [1, 2, 3, 4, 1, 2, 3, 4];
 
   return (
-    <div className="spinner">
-      <h3>Homepage</h3>
-      <RingLoader color="#5D5FEF" size={400} />
+    <div className="w-75 mx-auto">
+      <h3>My class</h3>
+      <div className="d-flex my-3 flex-wrap justify-content-between">
+        {classData.map((data) => (
+          <div className={`${styles["class-content-container"]} mt-4 rounded-3`}>
+            <div className={`${styles["class-title-container"]} rounded-top-3`}> 
+              <h2 className={`${styles['class-title']} px-3 pt-3`}>2310-CLC-AWP-20KTPM2</h2>
+              <div className={`${styles['class-instructor']} px-3 pt-0`}>Advanced Web Programming</div>
+              <div className={`${styles['class-instructor']} px-3 pt-2 pb-3`}>Gia Huy</div>
+            </div>
+            <div className={`${styles["class-mid-container"]} rounded-top-3`}>
+              <h5 className={`${styles['class-assignment-title']} px-3 pt-3`}>Assignment - Due in January</h5>
+              <div className={`${styles['class-instructor']} px-3 pb-1`}>Infinite Scroll</div>
+              <div className={`${styles['class-instructor']} px-3 pb-1`}>Midterm Project Authentication</div>
+              <div className={`${styles['class-instructor']} px-3 pb-1`}>Final Project Classroom</div>
+            </div>
+            <div className={`${styles["class-footer-container"]} d-flex justify-content-end align-items-center px-4`}>
+              <FolderIcon className={`${styles["size-icon"]} m-3`}/>
+              <LeaveIcon className={styles["size-icon"]}/>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- FRONTEND/customer/src/components/Layout/MainNavigation.js
+++ FRONTEND/customer/src/components/Layout/MainNavigation.js
@@ -7,26 +7,31 @@ import "./MainNavigation.css";
 
 import Navbar from "react-bootstrap/Navbar";
 import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
 
 import User from "../../assests/img/user.jpg";
 import { ReactComponent as LogoutIcon } from "../../assests/svg/logout.svg";
 
-import axios from 'axios';
+import axios from "axios";
 
 const MainNavigation = () => {
   const authCtx = useContext(AuthContext);
+  const [active, setActive] = useState("myclass");
+
   const navigate = useNavigate();
   const isLoggedIn = authCtx.isLoggedIn;
 
   const userData = authCtx.userData;
 
   const token = authCtx.token;
-  const headers = { 'Authorization': `Bearer ${token}` };
+  const headers = { Authorization: `Bearer ${token}` };
   useEffect(() => {
-    if(authCtx.isLoggedIn){
-      const headers = { 'Authorization': `Bearer ${token}` };
-      axios.get(process.env.REACT_APP_API_HOST + 'auth/get-user', { headers })
-          .then(res => authCtx.setUserDataContext(res.data.data));
+    if (authCtx.isLoggedIn) {
+      const headers = { Authorization: `Bearer ${token}` };
+      axios
+        .get(process.env.REACT_APP_API_HOST + "auth/get-user", { headers })
+        .then((res) => authCtx.setUserDataContext(res.data.data));
     }
   }, []);
 
@@ -36,14 +41,31 @@ const MainNavigation = () => {
   };
 
   return (
-    <Navbar className="justify-content-between nav-container">
-      <h4
+    <Navbar
+      className="justify-content-between nav-container"
+      data-bs-theme="dark"
+    >
+      {/* <h4
         onClick={() => {
           navigate("/");
         }}
       >
         Authentication
-      </h4>
+      </h4> */}
+      <Container className="sub-nav-container p-0 m-0">
+        <Navbar.Brand href="/" className="nav-brand-container">
+          AWClassroom
+        </Navbar.Brand>
+        <Nav
+          className="me-auto"
+          activeKey={active}
+          onSelect={(selectedKey) => setActive(selectedKey)}
+        >
+          <Nav.Link href="/" eventKey="myclass">My class</Nav.Link>
+          <Nav.Link href="/" eventKey="todo">To do</Nav.Link>
+          <Nav.Link href="/" eventKey="schedule">Schedule</Nav.Link>
+        </Nav>
+      </Container>
 
       {!authCtx.isLoggedIn ? (
         <div className="d-flex">
@@ -68,14 +90,19 @@ const MainNavigation = () => {
         </div>
       ) : (
         <div className="d-flex align-items-center">
-          <div onClick={()=>{navigate('./profile')}} className="d-flex info-ava">
+          <div
+            onClick={() => {
+              navigate("./profile");
+            }}
+            className="d-flex info-ava"
+          >
             <div className="info-container">
               <div className="role">Hello,</div>
               <div className="username">{userData.username}</div>
             </div>
-            <img src={User} className="img-container"/>
+            <img src={User} className="img-container" />
           </div>
-          <LogoutIcon onClick={logoutHandler} className="icon"/> 
+          <LogoutIcon onClick={logoutHandler} className="icon" />
         </div>
       )}
     </Navbar>
